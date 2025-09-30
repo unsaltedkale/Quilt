80331f8c9d70b03c2c7f5c907262032e48c11316
Andrew Platosh <aplatosh6982@student.pps.net>
2025-09-30 13:08:32 -0700

    Minor Projectile Work

--- Andy's Path/Scripts/Projectile.gd
+++ Andy's Path/Scripts/Projectile.gd
@@ -2,16 +2,20 @@ extends Node
 
 var speed = 20
 var mousePosition
+var pressedPosition;
 var direction
 var isPressed = false
 
 func _process(delta: float) -> void:
 	print(isPressed)
-	if isPressed == true: 
-		direction = mousePosition - self.position
+	if isPressed == true:
+		direction = pressedPosition - self.position
 		self.position += speed * direction.normalized()
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		isPressed = true
-		mousePosition = event.position
-	
+		if event.pressed:
+			isPressed = true
+			mousePosition = event.position
+			pressedPosition = mousePosition
+		else:
+			isPressed = false
