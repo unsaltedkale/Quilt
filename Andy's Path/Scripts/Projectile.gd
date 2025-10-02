6b356fb8fb3af3dd47961b6f9f9123a94e4f7ae6
Andrew Platosh <aplatosh6982@student.pps.net>
2025-10-02 13:42:02 -0700

    Projectile

--- Andy's Path/Scripts/Projectile.gd
+++ Andy's Path/Scripts/Projectile.gd
@@ -2,15 +2,19 @@ extends Node
 
 var speed = 20
 var mousePosition
-var pressedPosition;
-var direction
+var pressedPosition
+var direction = Vector2.ZERO
 var isPressed = false
+var acceleration = Vector2(0, 9.8)
+var velocity = Vector2.ZERO
 
-func _process(delta: float) -> void:
-	print(isPressed)
+func _process(_delta: float) -> void:
 	if isPressed == true:
 		direction = pressedPosition - self.position
-		self.position += speed * direction.normalized()
+	if direction != Vector2.ZERO:
+		velocity += speed * direction.normalized() * _delta
+		velocity += acceleration * _delta
+		self.position += velocity
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed:
