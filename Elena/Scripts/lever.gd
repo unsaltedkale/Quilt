5c1fb6bfc8acd3fe4eac2ece44f5c763b6e1447d
elena-seneff <eseneff8925@student.pps.net>
2025-10-24 09:40:09 -0700

    started levers

--- Elena/Scripts/lever.gd
+++ Elena/Scripts/lever.gd
@@ -2,5 +2,20 @@ extends Node2D
 
 @export var state = "0"
 
-
-	
+func _ready():
+	if state == "0":	
+		$AnimatedSprite2D.play("lever_0") 
+	else:
+		$AnimatedSprite2D.play("lever_1")
+		
+func _procces():
+	if $Area2D.overlaps_body("Player") and Input.is_action_just_pressed("use"):
+		if state == "0":
+			print("state 1")
+			state == "1"
+			$AnimatedSprite2D.play("lever_1")
+		else: 
+			print("state 0")
+			state == "0"
+			$AnimatedSprite2D.play("lever_0")
+				
--- Emily WIP/Scripts/jump_component.gd
+++ Emily WIP/Scripts/jump_component.gd
@@ -13,7 +13,7 @@ func handle_jump(body: CharacterBody2D, want_to_jump:bool) -> void:
 		body.velocity.y = jump_velocity
 	is_jumping = body.velocity.y > 0 and not body.is_on_floor()
 	is_falling = body.velocity.y < 0 and not body.is_on_floor()
-	is_landing = body.was_on_floor()
+	#is_landing = body.was_on_floor()
 	
 	if is_jumping:
 		$"../AnimatedSprite2D".play("jump")
