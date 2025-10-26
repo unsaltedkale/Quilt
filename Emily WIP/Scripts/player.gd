8aac5e34479171ab068289cc59e3ffb9110b45f5
ItsAMeMario8 <emilyhwhyms1@gmail.com>
2025-10-25 21:25:36 -0700

    player fully works
    land animation is FAST

--- Emily WIP/Scripts/player.gd
+++ Emily WIP/Scripts/player.gd
@@ -5,9 +5,11 @@ extends CharacterBody2D
 @export var input_component: InputComponent
 @export var movement_component: MovementComponent
 @export var jump_component: JumpComponent
-@export var recoil_component: RecoilComponent
+@onready var projectile_scene = preload("res://Emily WIP/Scenes/red_projectile.tscn")
 
 var health_script: Node
+var speed = 150
+var player_direction
 
 func _ready():
 	health_script = $PlayerHealth  
@@ -17,22 +19,26 @@ func _physics_process(delta: float) -> void:
 	gravity_component.handle_gravity(self, delta)
 	movement_component.handle_horizontal_movement(self, input_component.input_horizontal)
 	jump_component.handle_jump(self, input_component.get_jump_input())
-	recoil_component.handle_movement(self, input_component.get_shoot_input())
 
 	move_and_slide()
+	
+	if Input.is_action_just_pressed("fire_projectile") and can_shoot:
+		shoot()
+		recoil()
 
 #player ability function
 var can_shoot: bool = true
 
 func take_damage(amount: int) -> void:
 	health_script.reduce_health(amount)
-	
-func _process(delta):
-	if Input.is_action_just_pressed("fire_projectile") and can_shoot:
-		shoot()
-		
+
 func shoot():
-	var proj = preload("res://Emily WIP/Scenes/red_projectile.tscn").instantiate()
-	owner.add_child(proj)
-	proj.transform = $Marker2D.global_transform
+	var proj = projectile_scene.instantiate()
+	proj.position = position
+	get_parent().add_child(proj)
+	proj.projectile_direction = (position - get_global_mouse_position()).normalized()
 	can_shoot = true
+
+func recoil():
+	player_direction = (position - get_global_mouse_position()).normalized()
+	position += player_direction * speed 
--- Emily WIP/Scripts/red_projectile.gd
+++ Emily WIP/Scripts/red_projectile.gd
@@ -1,16 +1,16 @@
 class_name ProjectileComponent
 extends Area2D
 
-var speed = .05
+var speed = 500
+var projectile_direction
 
-func _physics_process(delta):
-	#go to mouse location
-	position.x += get_viewport().get_mouse_position().x * speed
-	#print("projectile position x: ", position.x)
-	position.y += get_viewport().get_mouse_position().y * speed 
-	#print("projectile position y: ", position.y)
+func _process(delta):
+	position -= projectile_direction * speed * delta
+	pass
 
 func _on_projectile_body_entered(body):
 	if (body is TileMapLayer):
 		body.queue_free()
 		queue_free()
+	else:
+		pass
