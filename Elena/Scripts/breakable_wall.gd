d357bca75c53f93c27d6f86dccf58612acbc93e8
elena-seneff <eseneff8925@student.pps.net>
2025-11-14 09:43:07 -0800

    more breakable wall stuff

--- Elena/Scripts/breakable_wall.gd
+++ Elena/Scripts/breakable_wall.gd
@@ -1,13 +1,11 @@
 extends StaticBody2D
 
-@onready var timer: Timer 
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Projectile"):
-		timer.start()
+		_timer()
 		
 		
 func _timer() -> void:
 	await get_tree().create_timer(.5).timeout
-	print("Destroying Falling Object")
+	print("Destroying Wall")
 	get_parent().queue_free()
