c8518823737707d911117b1220abbc74b2995e19
Andrew Platosh <aplatosh6982@student.pps.net>
2025-11-17 13:34:49 -0800

    Camera Fixed

--- Andy's Path/Scripts/CameraController.gd
+++ Andy's Path/Scripts/CameraController.gd
@@ -1,33 +1,26 @@
 extends Camera2D
 
-var initialPosition
-var offset1 = 500
+var offset1 = 1000 #screen bounds
 
 var playerReference
 var screenIsMoving
 
-var target
-
 func _ready() -> void:
-	initialPosition = self.global_position
 	screenIsMoving = false
 	
 	playerReference = $"../Player"
 	
 func _process(delta: float) -> void:
-	
+	#Horizontal Movement
 	self.global_position.x = lerp(self.global_position.x, playerReference.global_position.x, delta * 2)
+	
 	if screenIsMoving == false:
-		if playerReference.global_position.y <= initialPosition.y - offset1:
-			target = playerReference.global_position.y
-			screenIsMoving = true
-		elif playerReference.global_position.y >= initialPosition.y + offset1:
-			target = playerReference.global_position.y
+		if abs(self.global_position.y - playerReference.global_position.y) > offset1:
 			screenIsMoving = true
 	else:
-		initialPosition = self.global_position
-		self.global_position.y = lerp(self.global_position.y, target, delta * 5)
-		print(abs(self.global_position.y - target))
-		if abs((self.global_position.y - target)) < 10:
-			self.position.y = target
+		#Vertical Movement
+		self.global_position.y = lerp(self.global_position.y, playerReference.global_position.y, delta * 10)
+		
+		#Move to target position if magnitude of distance < 10
+		if abs((self.global_position.y - playerReference.global_position.y)) < 5:
 			screenIsMoving = false
