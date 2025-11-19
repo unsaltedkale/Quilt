b5e6166c26931977df10acf43d09ccde5c30a5a3
ItsAMeMario8 <emilyhwhyms1@gmail.com>
2025-11-19 13:39:33 -0800

    wall jump

--- Elena/Scripts/wall_stick_component.gd
+++ Elena/Scripts/wall_stick_component.gd
@@ -4,10 +4,19 @@ extends Node
 @export_subgroup("Settings")
 
 var temp_acc = 1.2
+var wall_stick: bool = false
 
 func handle_wall(body: CharacterBody2D, _delta: float):
 	if body.is_on_wall() and not body.is_on_floor():
-		body.velocity.y = 50 * temp_acc
+		if Input.is_action_pressed("move_left") or Input.is_action_pressed("move_right"):
+			wall_stick = true
+			print("wall stick true")
+		else:
+			wall_stick = false
+	else:
+		wall_stick = false
+	if wall_stick:
+		body.velocity.y += 50 * temp_acc
 		temp_acc += 0.1
 		if body.velocity.y > 240: # some numer
 			body.velocity.y = 240
