d1b4c41f16c19644329a3267d8ee39113685bf03
ItsAMeMario8 <emilyhwhyms1@gmail.com>
2025-11-21 14:00:55 -0800

    recoil going in direction of mouse...

---
+++ Emily WIP/Scripts/projectile_area2d.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _on_projectile_body_entered(body): # has to be some kinematic body to queue or disable
+	if (body is TileMapLayer):
+		get_parent().queue_free()
+	else:
+		pass
--- Emily WIP/Scripts/red_projectile.gd
+++ Emily WIP/Scripts/red_projectile.gd
@@ -1,16 +1,9 @@
 class_name ProjectileComponent
-extends Area2D
+extends RigidBody2D
 
 var speed = 500
-var projectile_direction
+var projectile_direction 
 
 func _process(delta):
 	position -= projectile_direction * speed * delta
 	pass
-
-func _on_projectile_body_entered(body):
-	if (body is TileMapLayer):
-		body.queue_free()
-		queue_free()
-	else:
-		pass
