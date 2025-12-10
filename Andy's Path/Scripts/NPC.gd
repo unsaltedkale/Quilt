0f2d74b5aca62ed54d2cfbab02262b10084c9157
Andrew Platosh <aplatosh6982@student.pps.net>
2025-12-10 13:05:54 -0800

    Updated NPC

--- Andy's Path/Scripts/NPC.gd
+++ Andy's Path/Scripts/NPC.gd
@@ -3,12 +3,15 @@ extends Node2D
 var labelReference
 var areaReferecnce
 
+var isPressed = false
+
 func _ready() -> void:
 	labelReference = $Label
 	areaReferecnce = $Area2D
 
 func _process(delta: float) -> void:
 	Display_Text_With_Input()
+	print(isPressed)
 	
 func Display_Text_No_Input():
 	if areaReferecnce.isInTrigger == true:
@@ -20,10 +23,17 @@ func Display_Text_No_Input():
 		labelReference.visible = false
 		
 func Display_Text_With_Input():
-	if areaReferecnce.isInTrigger == true && Input.is_action_pressed("interact"):
-		print("In Trigger")
+	if isPressed == true:
 		labelReference.visible = true
-		
 	else:
-		print("Out of Trigger")
 		labelReference.visible = false
+	if areaReferecnce.isInTrigger == true:
+		#print("In Trigger")
+		if Input.is_action_just_pressed("interact"):
+			if isPressed == true:
+				isPressed = false
+			else:
+				isPressed = true
+	else:
+		pass
+		#print("Out of Trigger")
