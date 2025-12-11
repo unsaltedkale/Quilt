e466dbaf3185c559a208c8fc676c3c38db89a2a2
elena-seneff <eseneff8925@student.pps.net>
2025-12-11 09:17:40 -0800

    worked more on breakable wall

--- Elena/Scripts/breakable_wall.gd
+++ Elena/Scripts/breakable_wall.gd
@@ -1,7 +1,8 @@
 extends StaticBody2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Projectile"):
+	if body.is_in_group("Projectile"): 
+		print("projectile hit breakable wall")
 		_timer()
 		
 		
