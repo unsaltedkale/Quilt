1f099b0caef3a4fc74c6915abf07436e27df2860
Andrew Platosh <aplatosh6982@student.pps.net>
2026-01-07 13:55:06 -0800

    camera zoom start

--- Andy's Path/Scripts/NPC.gd
+++ Andy's Path/Scripts/NPC.gd
@@ -2,12 +2,14 @@ extends Node2D
 
 var labelReference
 var areaReferecnce
+var cameraReference
 
 var isPressed = false
 
 func _ready() -> void:
 	labelReference = $Label
 	areaReferecnce = $Area2D
+	cameraReference = $"../../Camera2D"
 
 func _process(delta: float) -> void:
 	Display_Text_With_Input()
@@ -23,17 +25,15 @@ func Display_Text_No_Input():
 		labelReference.visible = false
 		
 func Display_Text_With_Input():
-	if isPressed == true:
-		labelReference.visible = true
-	else:
-		labelReference.visible = false
 	if areaReferecnce.isInTrigger == true:
-		#print("In Trigger")
 		if Input.is_action_just_pressed("interact"):
 			if isPressed == true:
 				isPressed = false
 			else:
 				isPressed = true
+				
+	if isPressed == true:
+		labelReference.visible = true
+		cameraReference.Zoom(0.5,0.5)
 	else:
-		pass
-		#print("Out of Trigger")
+		labelReference.visible = false
