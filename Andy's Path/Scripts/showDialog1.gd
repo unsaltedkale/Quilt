acc37f5c5aa5b8f3b91b0e7d3cf10f6efbf6d676
Andrew Platosh <aplatosh6982@student.pps.net>
2026-01-15 14:07:22 -0800

    improvements on dialogue

--- Andy's Path/Scripts/showDialog1.gd
+++ Andy's Path/Scripts/showDialog1.gd
@@ -4,4 +4,9 @@ extends Button
 @export var dialogFolder = load("res://Andy's Path/Resources/ExampleCharacterLines.tres")
 
 func _ready() -> void:
-	text = dialogFolder.text["0"]
+	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+	pressed.connect(_on_pressed)
+
+func _on_pressed() -> void:
+	for i in dialogFolder.text:
+		text = text + dialogFolder.text[str(i)]
