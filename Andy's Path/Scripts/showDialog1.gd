05a0d576d475a5c68d2676193fbbbfc749fca21c
Andrew Platosh <aplatosh6982@student.pps.net>
2026-01-20 13:55:16 -0800

    Dialogue system done

--- Andy's Path/Scripts/showDialog1.gd
+++ Andy's Path/Scripts/showDialog1.gd
@@ -2,11 +2,27 @@ extends Button
 
 
 @export var dialogFolder = load("res://Andy's Path/Resources/ExampleCharacterLines.tres")
+var text_list = []
+var char_list = []
+var dialogue_counter = 0
+var isTyping = false
+var line = []
 
+func _process(delta: float) -> void:
+	print(isTyping)
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_WHEN_PAUSED
 	pressed.connect(_on_pressed)
-
+	line = dialogFolder.text[str(dialogue_counter)]
 func _on_pressed() -> void:
-	for i in dialogFolder.text:
-		text = text + dialogFolder.text[str(i)]
+	if not isTyping:
+		text = ''
+		isTyping = true
+		dialogue_counter += 1
+		for character in range(len(line)):
+			text = text + line[character]
+			await wait(0.1)
+		isTyping = false
+
+func wait(duration):
+	await get_tree().create_timer(duration).timeout
