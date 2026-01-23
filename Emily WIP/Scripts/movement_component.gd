d120048e678277c03b2831aa0c3e4ffd58f02d9e
Hestia <abuckley0278@student.pps.net>
2026-01-23 10:53:42 -0800

    movement is back and cutscene freeze works

--- Emily WIP/Scripts/movement_component.gd
+++ Emily WIP/Scripts/movement_component.gd
@@ -6,20 +6,30 @@ extends Node
 @export var jump_component: JumpComponent
 @onready var Player = get_parent()
 @export var prev_player_is_cutscene: bool
+@export var cutscene_frozen: bool
 
 var previous_velocity = 0
 
 func _ready():
 	prev_player_is_cutscene = false
 
+func _process(delta) -> void:
+	if prev_player_is_cutscene != null && Player.is_cutscene != null:
+		print("beep")
+		if Player.is_cutscene != prev_player_is_cutscene:
+				print("boop")
+				Player.velocity.x = 0
+				prev_player_is_cutscene = Player.is_cutscene
+				cutscene_frozen = true
+		elif Player.is_cutscene == false:
+			cutscene_frozen = false
+			pass
+
 func handle_horizontal_movement(body: CharacterBody2D, direction: float) -> void:
 	#if recoil, launch in air a lil
 	var delta: float = get_process_delta_time()
 	var traction = 2500
-	if prev_player_is_cutscene != null && Player.is_cutscene != null:
-		if Player.is_cutscene != prev_player_is_cutscene:
-				body.velocity.x = 0
-	else:
+	if cutscene_frozen == false:
 		body.velocity.x = move_toward(body.velocity.x, direction * speed, delta * traction)
-	prev_player_is_cutscene = Player.is_cutscene
+	
 	
