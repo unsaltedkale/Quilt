9f63da28630b431c575a7c9c456f1c096065eea8
ItsAMeMario8 <emilyhwhyms1@gmail.com>
2026-02-02 13:49:18 -0800

    corner jump fix?

--- Emily WIP/Scripts/jump_component.gd
+++ Emily WIP/Scripts/jump_component.gd
@@ -26,9 +26,10 @@ func handle_jump(body: CharacterBody2D, want_to_jump:bool) -> void:
 	if Input.is_action_just_pressed("jump"):
 		if body.is_on_floor():
 			body.velocity.y = jump_velocity
-		if body.is_on_wall() and Input.is_action_pressed("move_right"):
-			body.velocity.y = wall_jump_vel
-			body.velocity.x = -wall_jump_push
-		if body.is_on_wall() and Input.is_action_pressed("move_left"):
-			body.velocity.y = wall_jump_vel
-			body.velocity.x = wall_jump_push
+		else:
+			if body.is_on_wall() and Input.is_action_pressed("move_right"):
+				body.velocity.y = wall_jump_vel
+				body.velocity.x = -wall_jump_push
+			if body.is_on_wall() and Input.is_action_pressed("move_left"):
+				body.velocity.y = wall_jump_vel
+				body.velocity.x = wall_jump_push
--- Emily WIP/Scripts/stasis.gd
+++ Emily WIP/Scripts/stasis.gd
@@ -5,13 +5,16 @@ extends Area2D
 func _process(_delta):
 	if player.is_suspended_stasis:
 		player.velocity = Vector2(0,0)
-		player.position = position
+		print("payer vel: ",player.velocity)
+		player.position = position # take out and gets right stasis, gravity active
+		print("stasis pos: ", position)
 
 func on_body_entered(area: Area2D):
 	if not player.is_suspended_stasis:
 		if area.is_in_group("Projectile") or area.is_in_group("Player"):
 			player.is_suspended_stasis = true
-			player.position = position
+			print("stasis pos: ", position)
+			player.position = position #in list of stasis, get collided stasis
 			player.collected_objects = player.max_stars
 	else:
 		if area.is_in_group("Projectile"):
