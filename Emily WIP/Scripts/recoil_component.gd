8ab54052cb2a866f7ce35d32342790e61fefda2b
Hestia <abuckley0278@student.pps.net>
2026-02-02 12:50:33 -0800

    recoil is cool now

--- Emily WIP/Scripts/recoil_component.gd
+++ Emily WIP/Scripts/recoil_component.gd
@@ -4,7 +4,7 @@ extends Node
 @export_subgroup("Settings")
 
 var can_shoot
-var force : Vector2 = Vector2(3000,4000) 
+var force : Vector2 = Vector2(2500,1500)
 var player_direction 
 var joystick_direction = Input.get_vector("recoil_left","recoil_right","recoil_up","recoil_down")
 
@@ -12,7 +12,7 @@ func handle_recoil(body: CharacterBody2D, direction: float):
 	can_shoot = body.can_shoot
 	if Input.is_action_just_pressed("fire_projectile") and can_shoot:
 		player_direction = body.get_local_mouse_position().normalized() #+ body.joystick_direction.normalized()
-		body.velocity -= player_direction * force
+		body.velocity = player_direction * force * -1
 		
 		#print("player direction ", player_direction, "player act pos ", body.global_position)
 	
