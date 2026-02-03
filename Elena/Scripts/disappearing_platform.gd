587f3af324e7c79148034246cacf1eb349bf50d6
elena-seneff <ewsews6@gmail.com>
2026-02-03 10:02:42 -0800

    updated disappearing platforms

--- Elena/Scripts/disappearing_platform.gd
+++ Elena/Scripts/disappearing_platform.gd
@@ -1,13 +1,10 @@
-extends StaticBody2D
+extends Area2D
 
-@onready var timer: Timer = $VanishingTimer
-@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
-
-func _on_trigger_area_body_entered(body: Node2D) -> void:
+func _on_body_entered(body: Node2D):
 	if body.is_in_group("Player"):
-		timer.start()
-
+		$Timer.start()
+		
 func _on_timer_timeout() -> void:
-	var tween = create_tween()
-	tween.tween_property(self, "modulate:a", 0, 0.4)
-	collision_shape_2d.position = Vector2(-10000, 20000)
+	var parent = self.get_parent()
+	if parent:
+		parent.modulate.a = 0
