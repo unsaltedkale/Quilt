2b72cfb4673eb0d7156dce715c3bdaf17829c019
elena-seneff <ewsews6@gmail.com>
2026-02-11 09:54:25 -0800

    2/11/26

--- Emily WIP/Scripts/red_projectile.gd
+++ Emily WIP/Scripts/red_projectile.gd
@@ -1,5 +1,6 @@
 extends Area2D
 @onready var player = $"../Player"
+@onready var mirror = $"../Mirror"
 var speed : Vector2 = Vector2(-2000,-2000)
 var projectile_direction
 
@@ -11,8 +12,14 @@ func _process(delta):
 func _on_projectile_entered(body:Node2D):
 	if body.is_in_group("tilemap"):
 		queue_free()
-		
+	
 func _on_area_entered(body: Area2D):
 	if not player.is_exiting_stasis and not player.is_suspended_zipline:
 		if body.is_in_group("Stasis"):
 			queue_free()
+			
+		
+
+		
+		
+	
