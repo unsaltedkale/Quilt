e8d48e5a86081ec1b1c817ff98f561c767ffcbf8
Hestia <abuckley0278@student.pps.net>
2026-02-19 14:14:11 -0800

    added blue flow sprites and made star elevator work

--- Alex's Folder/cutscene_event_resources/dialouge_cs_event.gd
+++ Alex's Folder/cutscene_event_resources/dialouge_cs_event.gd
@@ -2,13 +2,12 @@
 extends cutscene_event
 class_name dialouge_cutscene_event
 
-@export var animator: NodePath
-@export var animation_name: String
-@export var horizontal_flip: bool
+@export var dia: DialogResource
 
 
 func execute(cutscene_trigger: Node) -> void:
 	#place holder
+	
 	print("dialouge here")
 	pass
 	
