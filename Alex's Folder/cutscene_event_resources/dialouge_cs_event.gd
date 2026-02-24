08c87062a1d13aa81b5941b219f599ee62a13b55
unsaltedkale <abuckley0278@student.pps.net>
2026-02-23 18:59:57 -0800

    new pyra sprite, more of first cutscene finished

--- Alex's Folder/cutscene_event_resources/dialouge_cs_event.gd
+++ Alex's Folder/cutscene_event_resources/dialouge_cs_event.gd
@@ -2,15 +2,15 @@
 extends cutscene_event
 class_name dialouge_cutscene_event
 
-@export var dia: DialogResource
+@export var dia: Resource
 
 
 func execute(cutscene_trigger: Node) -> void:
 	#place holder
 	
-	var button = cutscene_trigger.get_tree().get_nodes_in_group("Conductor")[0]  
+	var button = cutscene_trigger.get_tree().get_first_node_in_group("Dialouge_Button") 
 	
-	button.Dialouge(dia)
+	button.Dialogue(dia.resource_path)
 	
 	await button.dialouge_finished
 	
