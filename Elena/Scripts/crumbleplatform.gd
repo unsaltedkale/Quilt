6fe7cbe93075132e5cf5ea52d3d0105f5fddb9ed
elena-seneff <ewsews6@gmail.com>
2026-03-09 09:48:51 -0700

    3/9/26

--- Elena/Scripts/crumbleplatform.gd
+++ Elena/Scripts/crumbleplatform.gd
@@ -3,6 +3,8 @@ extends RigidBody2D
 @onready var timer_length: float = 1.0
 @onready var Player: CharacterBody2D = $"../Player"
 
+#Could do Timer or Area2D, need to see which would be better for how we want to use them
+
 func _ready() -> void:
 	self.gravity_scale = 0
 	#$Timer.start()
--- Emily WIP/Scripts/red_projectile.gd
+++ Emily WIP/Scripts/red_projectile.gd
@@ -11,11 +11,11 @@ func _process(_delta):
 func _physics_process(delta) -> void:
 	rotation = projectile_direction.angle()
 	position += projectile_direction * speed * delta
-	
-
+		
 func _on_projectile_entered(body:Node2D):
 	if body.is_in_group("tilemap"):
 		queue_free()
+		
 		
 func _on_area_entered(body: Area2D):
 	if body.is_in_group("Stasis"):
