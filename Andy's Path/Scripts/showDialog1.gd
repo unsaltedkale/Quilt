23cfe2f4270f59db744f437e568640f0f0dbc0f5
Andrew Platosh <aplatosh6982@student.pps.net>
2026-03-12 13:17:49 -0700

    Dialogue updated

--- Andy's Path/Scripts/showDialog1.gd
+++ Andy's Path/Scripts/showDialog1.gd
@@ -6,17 +6,19 @@ var text_list = []
 var char_list = []
 var line = []
 var dialogue_counter
-var isTyping = false
+var isTyping
+var font_size = 30
 
 signal dialouge_finished
 
 func _ready() -> void:
 	dialogue_counter = 0
+	$"../Dialogue".add_theme_font_size_override("font_size", font_size)
 func _on_pressed() -> void:
-	$"../Dialogue".visible_characters = 0
 	if dialogFolder != null:
 		line = dialogFolder.text[str(dialogue_counter)]
 	if not isTyping:
+		$"../Dialogue".visible_characters = 0
 		isTyping = true
 		text = line
 		for i in len(line):
@@ -30,7 +32,6 @@ func _on_pressed() -> void:
 		isTyping = false
 		dialogue_counter += 1
 func _process(delta: float) -> void:
-	#print("print: " + str(dialogue_counter))
 	if Input.is_action_pressed("interact"):
 		Dialogue("res://Alex's Folder/cutscene_event_resources/cutscenes/crypt_fall_cutscene/dia resources/crypt_fall_dialouge.tres")
 func wait(duration):
