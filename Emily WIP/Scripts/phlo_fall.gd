48c208dee835f535809828efcf5aec8146dd7439
ItsAMeMario8 <emilyhwhyms1@gmail.com>
2026-03-16 13:09:23 -0700

    phlo can't fall

--- Emily WIP/Scripts/phlo_fall.gd
+++ Emily WIP/Scripts/phlo_fall.gd
@@ -6,19 +6,15 @@ var fall_timer: float = 0
 func Enter():
 	fall_timer = 0
 
-func _process(delta: float) -> void:
-	Update(delta)
 func Update(_delta):
 	fall_timer += 20 * _delta
-	if player.velocity.y >= 0:
-		an.play("phlo_fall")
 	if player.velocity.y < 0:
 		an.play("phlo_jump")
+	if player.velocity.y >= 0:
+		an.play("phlo_fall")
 	if player.is_on_floor():
 		an.play("phlo_land")
 		#phlo_wump when large fall
-func _physics_process(_delta) -> void:
-	Physics_Update(_delta)
 func Physics_Update(_delta):
 	player.velocity.y += gravity * _delta
 	if player.is_on_floor():
