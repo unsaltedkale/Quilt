d3a93a90b7002af27edcaaeec73904ed104fad9b
unsaltedkale <abuckley0278@student.pps.net>
2026-03-21 19:06:18 -0700

    air movement is better? i think?

--- Emily WIP/Scripts/state_base.gd
+++ Emily WIP/Scripts/state_base.gd
@@ -8,6 +8,8 @@ var move_dir
 var recoil: bool
 var cutscene: bool
 @export var state_name: String
+var acceleration = 1500
+var decceleration = 750
 
 signal Transition
 
@@ -29,7 +31,9 @@ func _physics_process(_delta) -> void:
 	if player.find_child("StateMachine").current_state.state_name != "cutscene" && player.find_child("StateMachine").current_state.state_name != "recoil":
 		move_dir = Input.get_axis("move_left","move_right")
 		if move_dir !=0:
-			player.velocity.x = move_dir * speed
+			player.velocity.x = move_toward(player.velocity.x, move_dir * speed, acceleration * _delta)
+		elif move_dir == 0:
+			player.velocity.x = move_toward(player.velocity.x, 0, decceleration * _delta)
 	if player.is_phlo:
 		#if find_parent("StateMachine").current_state.state_name != "phlo_walk" || find_parent("StateMachine").current_state.state_name != "phlo_fall" || find_parent("StateMachine").current_state.state_name != "phlo_jump" || find_parent("StateMachine").current_state.state_name != "phlo_cutscene":
 			#Transition.emit(self,"phlowalk")
