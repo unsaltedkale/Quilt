909de1a0dfb03beca927ccf731d0fd0a655d584b
ItsAMeMario8 <emilyhwhyms1@gmail.com>
2026-03-31 13:54:14 -0700

    player isnt stuck in one block
    start wall stick and jump

---
+++ Emily WIP/Scripts/wall_stick.gd
@@ -0,0 +1,20 @@
+extends State
+
+func Enter():
+	'''if body.is_on_wall() and not body.is_on_floor():
+		if p_moving and not player.is_magical_wall:
+			wall_stick = true
+		elif p_moving and player.is_magical_wall:
+			wall_stick = false
+	else:
+		wall_stick = false
+	if wall_stick:
+		body.velocity.y += 50 * temp_acc
+		temp_acc += 0.1
+		if body.velocity.y > 240: # some numer
+			body.velocity.y = 240
+		print("body hit wall")
+	else:
+		pass'''
+	pass
+	
