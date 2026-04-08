8b08179cf9daba2a380646e7aa303741e5dcbf8e
unsaltedkale <abuckley0278@student.pps.net>
2026-04-08 11:19:54 -0700

    renamed jump force to jump force y, figured out how to determine wall direction kinda for wall jump

--- Emily WIP/Scripts/wall_jump.gd
+++ Emily WIP/Scripts/wall_jump.gd
@@ -1,13 +1,13 @@
 extends State
-@export var jump_force: float
+@export var jump_force_y: float
 @export var jump_force_x: int
 var timer: float
 
 func quilt_wall_jump():
 	if not player.is_on_wall():
-		player.velocity.y -= jump_force
+		player.velocity.y -= jump_force_y
 	else:
-		player.velocity.y -= jump_force / 3
+		player.velocity.y -= jump_force_y / 3
 	player.velocity.x += jump_force_x
 	if player.is_phlo:
 		an.play("phlo_jump")
@@ -19,12 +19,13 @@ func Enter():
 	quilt_wall_jump()
 
 func Physics_Update(_delta):
+	
 	if timer <= 0:
-		print_debug("click")
+		#print_debug("click")
 		Transition.emit(self,"fall")
 	else:
 		quilt_wall_jump()
-		print_debug("tap")
+		#print_debug("tap")
 		timer -= 1
 	if Input.is_action_just_pressed("fire_projectile") || Input.is_action_just_pressed("recoil_left") || Input.is_action_just_pressed("recoil_right") || Input.is_action_just_pressed("recoil_up") || Input.is_action_just_pressed("recoil_down"):
 		if player.collected_objects != 0:
--- Emily WIP/Scripts/wall_stick.gd
+++ Emily WIP/Scripts/wall_stick.gd
@@ -18,10 +18,21 @@ func Enter():
 		pass
 
 func Physics_Update(_delta):
+	
+	#THIS is how you get the wall direction?????? PAIN!!!!!!!
+	
+	'''if player.get_slide_collision_count() != 0:
+		for i in player.get_slide_collision_count():
+			print(i)
+			var collision = player.get_slide_collision(i)
+			print("Collided with: ", collision.get_collider().get_path())
+			print("Collided with: ", collision.get_normal())'''
+	
 	# wall cling storage fix:
 	if not player.is_on_wall() and not player.is_on_floor():
 		wall_stick = false
 		Transition.emit(self, "idle")
+	
 	if Input.is_action_just_pressed("jump"):
 		wall_stick = false
 		Transition.emit(self, "walljump")
