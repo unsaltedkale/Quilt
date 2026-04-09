f88cc776ce4069080fb8d1422e63902c53832b82
elena-seneff <ewsews6@gmail.com>
2026-04-09 08:58:28 -0700

    Fixed Breakable Wall

--- Elena/Scripts/breakable_area.gd
+++ Elena/Scripts/breakable_area.gd
@@ -1,7 +1,8 @@
 extends Area2D
 
-func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Projectile"):
+func _on_body_entered(body: Node2D) -> void:
+	
+	if body.is_in_group("Projectile"):
 		var parent = self.get_parent()
 		print("Projectile hit Breakable Wall")
 		if parent:
