5f32cf489b4157893c287836cf3a9892c4747d7f
Salem Silkie <144173130+Phendorana@users.noreply.github.com>
2026-04-10 13:54:39 -0700

    Dialog SFX Progress

--- Salem WIPs/dialogue voices.gd
+++ Salem WIPs/dialogue voices.gd
@@ -1,37 +1,39 @@
-extends Node
+extends AudioStreamPlayer
 
 var sd1 = get_parent()
 @export var voices : Dictionary[String, AudioStream]
 
 const dialouge_type = preload("res://Andy's Path/Scripts/showDialog1.gd").dialouge_type
 
-func _ready() -> void:
-	pass
+var pitch
+var newnote = 1
 
 func _voice_time(character_speaking: String, letter_displayed: String, line_type: dialouge_type):
-	print("cs: " + character_speaking)
-	print("ld: " + letter_displayed)
-	print("lt: " + dialouge_type.keys()[line_type])
-	
-	#does not make sound on capital letters. sometimes dialouge has captial letters. idk.
-	
-	match letter_displayed: #TODO:  SET PIITCH
-		"a":
-			print("it  a")
-		"e":
-			pass
-		"i":
-			pass
-		"o":
-			pass
-		"u":
-			pass
+	#print("cs: " + character_speaking)
+	#print("ld: " + letter_displayed)
+	#print("lt: " + dialouge_type.keys()[line_type]) #speech, thought, or narration
+	match letter_displayed:
+		"i", "I":
+			newnote -= 1
+			pitch_scale = 1
+		"a", "A":
+			newnote -= 1
+			pitch_scale = 0.9
+		"e", "E":
+			newnote -= 1
+			pitch_scale = 0.8
+		"o", "O":
+			newnote -= 1
+			pitch_scale = 0.6
+		"u", "U":
+			newnote -= 1
+			pitch_scale = 0.4
 		_:
-			return
-	for character in voices.keys():
-		if character == character_speaking:
-			var note = AudioStreamPlayer.new()
-			note.stream = voices[character]
-			note.autoplay = true
-			note.finished.connect(note.queue_free)
-			add_child(note)
+			pass
+	if newnote <= 0 and line_type == dialouge_type.speech:
+		for character in voices.keys():
+			if character == character_speaking:
+				stream = voices[character]
+		stop()
+		play()
+		newnote = 2 #how many vowels per note
