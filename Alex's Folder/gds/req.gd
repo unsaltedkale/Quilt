19b9f07786d7f17687b99f4c0b5b44748d42935c
unsaltedkale <abuckley0278@student.pps.net>
2026-04-24 15:30:22 -0700

    Added (changable) particles to Barriers and Mirror tilemaps

--- Alex's Folder/gds/req.gd
+++ Alex's Folder/gds/req.gd
@@ -3,17 +3,56 @@ extends Node2D
 enum req_type {staging, prod}
 
 @export var a: req_type = req_type.staging
+@export var has_set_tile_particles: bool = false
+
+@onready var mb_p = preload("res://Alex's Folder/tscns/Tile Particle Effects/magical_barrier_particle.tscn")
+@onready var umb_p = preload("res://Alex's Folder/tscns/Tile Particle Effects/unmagical_barrier_particle.tscn")
+@onready var mir_p = preload("res://Alex's Folder/tscns/Tile Particle Effects/mirror_particle.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
 	if a == req_type.staging:
 		if get_tree().get_nodes_in_group("Req").size() > 1:
 			print("REQ STAGING OF " + get_parent().name + " IN PROD, SELF DESTRUCTING!")
 			queue_free()
+		pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if has_set_tile_particles == false:
+		_set_tile_particles()
+		has_set_tile_particles = true
+			
+	pass
+	
+func _set_tile_particles() -> void:
+	var tiles = $"../Tiles"
+	print("setting...")
+	for tml in tiles.get_children():
+		
+		var p = null
+		
+		print(tml.name)
+		print(tml.get_used_cells())
+		match tml.name:
+			"Magical_Barrier":
+				p = mb_p
+			"Unmagical_Barrier":
+				p = umb_p
+			"Mirror":
+				p = mir_p
+		
+		if p != null:
+			for v in tml.get_used_cells():
+				
+				var localCellPosition = tml.map_to_local(v)
+				var globalCellPosition = tml.to_global(localCellPosition)
+				
+				var part = p.instantiate()
+				part.position = globalCellPosition
+				get_tree().current_scene.add_child(part)
+				pass
 		pass
 	pass
