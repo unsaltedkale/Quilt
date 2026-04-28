1a95debc02649d2be8be2757471cfd76e6430ee7
Andrew Platosh <aplatosh6982@student.pps.net>
2026-04-28 13:53:48 -0700

    camera chagnge trigger

--- Andy's Path/Scripts/CameraTrigger.gd
+++ Andy's Path/Scripts/CameraTrigger.gd
@@ -1,10 +1,21 @@
 extends Node
 
 @export var cameraOffset = 1
-@export var cameraZoom = 1
+@export var targetZoom = 0.5
+@export var zoomSpeed = 1
+
+@onready var cameraRef = $"../../Camera2D"
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.get_parent().is_in_group("Player"):
+	if body.is_in_group("Player"):
 		print("change camera")
-		$"../../Camera2D".zoom *= cameraZoom
-		
+		targetZoom = 0.25
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		print("change camera")
+		targetZoom = cameraRef.zoom.x
+
+func _process(delta: float) -> void:
+	cameraRef.zoom.x = lerp(cameraRef.zoom.x, targetZoom, delta*zoomSpeed)
+	cameraRef.zoom.y = lerp(cameraRef.zoom.y, targetZoom, delta*zoomSpeed)
