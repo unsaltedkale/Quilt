c159ebd347fc23fd790680bb18f3587e873f364b
Phendora <144971769+Phendora@users.noreply.github.com>
2026-04-29 13:34:58 -0700

    SFX work
    Added sfx to disappearing platforms and made sure all the sfx are going thru the sfx bus

--- Elena/Scripts/disappearing_platform.gd
+++ Elena/Scripts/disappearing_platform.gd
@@ -3,9 +3,11 @@ extends StaticBody2D
 var player
 var start_position: Vector2
 var start_rotation: float
-@export var time: float
+@export var time: float = 3.0
 
-var timer : SceneTreeTimer
+@onready var timer : Timer = $"Timer"
+
+@onready var crumble_sfx : AudioStreamPlayer = $"Crumbling"
 
 func _ready() -> void:
 	
@@ -16,7 +18,7 @@ func _ready() -> void:
 	
 	start_position = global_position
 	start_rotation = rotation
-	
+	timer.wait_time = time
 
 func _process(_delta: float) -> void:
 	if player != null:
@@ -27,25 +29,28 @@ func _process(_delta: float) -> void:
 			player = get_tree().get_first_node_in_group("Player")
 		else:
 			player = $"../Player"
-		
 	
+	#make the sfx fade out so it's basically gone by the time the platform disappears
+	if !timer.is_stopped(): crumble_sfx.volume_linear = 0.5 * (timer.time_left / time) ** 2
+
+
 func _on_timer_area_body_entered(body: Node2D) -> void:
-		if body.is_in_group("Player"):
-			timer = get_tree().create_timer(time)
-			await timer.timeout
-			set_collision_layer_value(1, false)
-			self.modulate.a = 0
+		if body.is_in_group("Player") && timer.is_stopped():
+			timer.start()
+			crumble_sfx.play()
 			#playanimationforplatformbreaking?
-		
+
+
 func reset_platforms():
-		if timer != null: timer.timeout.emit()
+		timer.stop()
+		timer.timeout.emit()
 		global_position = start_position
 		rotation = start_rotation
 		self.modulate.a = 1
 		set_collision_layer_value(1, true)
-		
-	
-	
-	
-	
-	
+
+
+func _on_timer_timeout() -> void:
+	set_collision_layer_value(1, false)
+	self.modulate.a = 0
+	crumble_sfx.stop()
