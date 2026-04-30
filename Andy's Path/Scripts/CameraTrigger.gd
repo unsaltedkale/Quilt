f83bb8acaaf24c6469ad11bcdc2d5280a0658a08
Andrew Platosh <aplatosh6982@student.pps.net>
2026-04-30 14:11:28 -0700

    Vignette 3+4

--- Andy's Path/Scripts/CameraTrigger.gd
+++ Andy's Path/Scripts/CameraTrigger.gd
@@ -4,7 +4,7 @@ extends Node
 @export var targetZoom = 0.5
 @export var zoomSpeed = 1
 
-@onready var cameraRef = $"../../Camera2D"
+@onready var cameraRef = $"../Camera2D"
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
