88d49b770c0be6b6ef24c7ce5331cf5bf7a13cf8
Andrew Platosh <aplatosh6982@student.pps.net>
2026-04-30 15:34:22 -0700

    Vignette 5

--- Andy's Path/Scripts/CameraTrigger.gd
+++ Andy's Path/Scripts/CameraTrigger.gd
@@ -1,21 +1,31 @@
 extends Node
 
-@export var cameraOffset = 1
+@export var cameraOffsetX = 0.0
+
+@export var cameraOffsetY = 0.0
 @export var targetZoom = 0.5
 @export var zoomSpeed = 1
 
 @onready var cameraRef = $"../Camera2D"
+@onready var inCameraTrigger = false
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		inCameraTrigger = true
 		print("change camera")
-		targetZoom = 0.25
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player"):
+		inCameraTrigger = false
 		print("change camera")
-		targetZoom = cameraRef.zoom.x
 
 func _process(delta: float) -> void:
-	cameraRef.zoom.x = lerp(cameraRef.zoom.x, targetZoom, delta*zoomSpeed)
-	cameraRef.zoom.y = lerp(cameraRef.zoom.y, targetZoom, delta*zoomSpeed)
+	if inCameraTrigger == true:
+		cameraRef.zoom.x = lerp(cameraRef.zoom.x, targetZoom, delta*zoomSpeed)
+		cameraRef.zoom.y = lerp(cameraRef.zoom.y, targetZoom, delta*zoomSpeed)
+		
+		cameraRef.global_position.x = lerp(cameraRef.global_position.x, cameraOffsetX, delta*zoomSpeed)
+		cameraRef.global_position.y = lerp(cameraRef.global_position.y, cameraOffsetY, delta*zoomSpeed)
+	else:
+		cameraRef.zoom.x = lerp(cameraRef.zoom.x, 0.5, delta*zoomSpeed)
+		cameraRef.zoom.y = lerp(cameraRef.zoom.y, 0.5, delta*zoomSpeed)
