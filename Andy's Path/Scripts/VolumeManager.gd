dc4d71fa3320ac60e63a9ec0302d9595b8ed6db9
Andrew Platosh <aplatosh6982@student.pps.net>
2026-05-01 20:24:18 -0700

    settings again

--- Andy's Path/Scripts/VolumeManager.gd
+++ Andy's Path/Scripts/VolumeManager.gd
@@ -1,10 +1,5 @@
 extends Node
 
-@onready var SoundFXSlider = $"../CanvasLayer/DialogueUi/UI/Settings/VolumeSettings/SoundFXSlider"
-@onready var GeneralSlider = $"../CanvasLayer/DialogueUi/UI/Settings/VolumeSettings/GeneralVolume"
-@onready var DialogueSlider = $"../CanvasLayer/DialogueUi/UI/Settings/VolumeSettings/DialogueSlider"
-#@onready var DialogueEchoSlider = $"../CanvasLayer/DialogueUi/UI/Settings/VolumeSettings/DialogueEchoSlider"
-@onready var MusicSlider = $"../CanvasLayer/DialogueUi/UI/Settings/VolumeSettings/MusicSlider"
 @onready var SFXBusIndex = AudioServer.get_bus_index("SFXMaster")
 @onready var GeneralBusIndex = AudioServer.get_bus_index("Master")
 @onready var DialogueBusIndex = AudioServer.get_bus_index("DialougeMaster")
@@ -12,8 +7,8 @@ extends Node
 @onready var MusicSliderBusIndex = AudioServer.get_bus_index("MusicMaster")
 
 func _process(_delta: float) -> void:
-	AudioServer.set_bus_volume_db(SFXBusIndex, SoundFXSlider.value)
-	AudioServer.set_bus_volume_db(GeneralBusIndex, GeneralSlider.value)
-	AudioServer.set_bus_volume_db(DialogueBusIndex, DialogueSlider.value)
-	#AudioServer.set_bus_volume_db(DialogueEchoBusIndex, DialogueEchoSlider.value)
-	AudioServer.set_bus_volume_db(MusicSliderBusIndex, MusicSlider.value)
+	AudioServer.set_bus_volume_db(SFXBusIndex, SettingsData.SFXVolumeValue)
+	AudioServer.set_bus_volume_db(GeneralBusIndex, SettingsData.generalVolumeValue)
+	AudioServer.set_bus_volume_db(DialogueBusIndex, SettingsData.dialogueVolumeValue)
+	AudioServer.set_bus_volume_db(MusicSliderBusIndex, SettingsData.MusicVolumeValue)
+	print(AudioServer.get_bus_volume_db(GeneralBusIndex))
